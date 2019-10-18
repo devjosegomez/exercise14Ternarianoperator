cd744d8710f0f9a12cb29a66deaf01ee832cff80
devjosegomez <devjosegomez@gmail.com>
2019-10-18 15:18:03 -0500

    Example ternary operator

--- src/com/exercise14Ternarianoperator/app/Ternarian.java
+++ src/com/exercise14Ternarianoperator/app/Ternarian.java
@@ -6,13 +6,14 @@ public class Ternarian {
 
 	public static void main(String[] args) {
 		int num1=0;
+		String message = "";
 		
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter a number: ");
 		
 		num1 = input.nextInt();
-		
-		
+		message = num1>5?"greater": "lower";
+		System.out.println(message);
 
 	}
 
