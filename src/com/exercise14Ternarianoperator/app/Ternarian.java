9a14fcc0901cfa4575370e4a428577e2bec39fda
devjosegomez <devjosegomez@gmail.com>
2019-10-18 15:13:11 -0500

    Initial commit

---
+++ src/com/exercise14Ternarianoperator/app/Ternarian.java
@@ -0,0 +1,19 @@
+package com.exercise14Ternarianoperator.app;
+
+import java.util.Scanner;
+
+public class Ternarian {
+
+	public static void main(String[] args) {
+		int num1=0;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		
+		num1 = input.nextInt();
+		
+		
+
+	}
+
+}
